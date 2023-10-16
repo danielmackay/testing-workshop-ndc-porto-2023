de7f94daf298d45ffd5cc5774f6eef60e8de6606
Elfocrash <nick.chapsas@gmail.com>
2023-10-16 09:48:48 +0100

    pre-ex

---
+++ 1.UnitTesting/1.Introduction/tests/AmazingCalculator.Tests.Unit/IntCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using AmazingCalculator;
+using FluentAssertions;
+using Xunit;
+
+namespace AmazingCalculator.Tests.Unit;
+
+public class IntCalculatorTests
+{
+    [Fact]
+    public void Add_ShouldAddTwoNumbers_WhenNumbersArePositiveIntegers()
+    {
+        // Arrange
+        var sut = new IntCalculator();
+        
+        // Act
+        var result = sut.Add(1, 2);
+
+        // Assert
+        result.Should().Be(3);
+    }
+}
